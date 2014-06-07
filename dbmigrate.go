396c456bb5d26a0743d44d8b913879d643d33341
Tanel Lebedev <tanel.lebedev@gmail.com>
2014-06-07 10:52:44 +0300

    Cassandra interface

--- dbmigrate.go
+++ dbmigrate.go
@@ -8,13 +8,72 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
+
+	"github.com/gocql/gocql"
 )
+
+// Database interface needs to be inmplemented to migrate a new type of database
 
 type Database interface {
 	CreateMigrationsTable() error
 	HasMigrated(filename string) (bool, error)
 	Migrate(filename string, migration string) error
 }
+
+// CassandraDatabase migrates Cassandra databases
+
+type CassandraDatabase struct {
+	readerSession *gocql.Session
+	writerSession *gocql.Session
+}
+
+func (cassandra *CassandraDatabase) CreateMigrationsTable() error {
+	/*
+		_, err := postgres.database.Exec("create table if not exists migrations(id serial, name text not null, created_at timestamp with time zone not null)")
+		if err != nil {
+			return err
+		}
+		_, err = postgres.database.Exec("create unique index idx_migrations_name on migrations(name)")
+		if err != nil {
+			if !strings.Contains(err.Error(), "already exists") {
+				return err
+			}
+		}
+	*/
+	return nil
+}
+
+func (cassandra *CassandraDatabase) HasMigrated(filename string) (bool, error) {
+	var count int
+	/*
+		err := postgres.database.QueryRow("select count(1) from migrations where name = $1", filename).Scan(&count)
+		if err != nil {
+			return false, err
+		}
+	*/
+	return count > 0, nil
+}
+
+func (cassandra *CassandraDatabase) Migrate(filename string, migration string) error {
+	/*
+		_, err := postgres.database.Exec(migration)
+		if err != nil {
+			return err
+		}
+		_, err = postgres.database.Exec("insert into migrations(name, created_at) values($1, current_timestamp)", filename)
+		return err
+	*/
+	return nil
+}
+
+func NewCassandraDatabase(readerSession *gocql.Session, writerSession *gocql.Session) *CassandraDatabase {
+	return &CassandraDatabase{
+		readerSession: readerSession,
+		writerSession: writerSession,
+	}
+}
+
+// PostgresDatabase migrates Postgresql databases
 
 type PostgresDatabase struct {
 	database *sql.DB
